9e7e51edd1c73ee22793f687b54e82fa6faa9670
farshid bahadori <farshidb2011@gmail.com>
2025-03-16 16:24:13 +0330

    Improve site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'http://localhost:4321/',
+  integrations: [vue({
+    appEntrypoint: '/src/vue_main.js'
+  })],
+  vite: {
+    resolve: {
+        alias: {
+            '@vcom' : '/src/components/vue/',
+            '@': 'src/'
+        }
+    }
+  }
+});
---
+++ src/components/vue/Greeting.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+import { ref } from 'vue';
+
+const counter = ref(0);
+
+
+const increment = () => {    
+    counter.value += 1;
+}
+</script>
+
+<template>
+    <button @click="increment">Increment</button>
+    <h1>Counter is {{ counter }}</h1>
+</template>
